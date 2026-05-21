41163c1762b136e018accf5e98ab937d27de96ca
Ayman Laamarti <aymanlaamarti00@gmail.com>
2026-05-21 18:55:58 +0200

    Limpieza: Eliminar VerificarRol.php duplicado y limpiar app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
-            'rol' => \App\Http\Middleware\VerificarRol::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
